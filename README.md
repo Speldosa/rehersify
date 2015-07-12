ffef03063d69e29050b809f719bd88991adab5ad
Martin Larsson <martinlarsson@Martins-MBP.lan>
2015-07-12 17:45:50 +0200

    Updated the program to alpha version 0.2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Rehersify
 
 # Version
-alpha 0.1
+alpha 0.2 (released 2015-07-12)
 
 # Author
 Martin Larsson, to.martin.larsson@gmail.com, [www.martinlarsson.net](http://www.martinlarsson.net).
@@ -13,21 +13,28 @@ GNU Gen­eral Public Li­cense, ver­sion 3
 # Description
 This is rehersify, a small [python](https://www.python.org) script that uses [SoX](http://sox.sourceforge.net/) to create rehersal music files out of individual music files containing single voices.
 
-The program is in the Alpha stage, meaning that the feature set is extremely limited, that the customization options are non-existent, that the progra is prone to break if not used exactly as intended, and that this readme file is incredibly sparse. However, 
+The program is in the Alpha stage, meaning that the feature set is extremely limited, that the customization options are non-existent, that the program is prone to break if not used exactly as intended, and that this readme file is incredibly sparse. However, given that one follows the instructions, it should be usable.
 
 # Dependencies
 In order to run this script you need [Bash](https://en.wikipedia.org/wiki/Bash_(Unix_shell)), [Python](https://www.python.org) and [Sox](http://sox.sourceforge.net/) installed and ready to go.
 
 # Basic usage
 ## Input files
-Input files should be named "This is the songs name @ voice-number" (the '-number' is optional) and placed in the folder 'Input'.
+Input files should be named "This is the songs name @ voice $ voicePlacement" (the ' $ voicePlacement' option is optional) and placed in the folder 'input'. Input formats that are accepted are wav and aif. The voice placement command tells the program in what order the different parts should be placed in stereo space. Lower numbers are placed more to the left and higher numbers are placed more to the right. Counting starts at "1".
+
 ## Output files
-Output files are generated in the 'Output' folder.
+Output files are generated in the 'output' folder. Output files have the format 'aif'.
+
 ## Operation
 When you've placed your input files in the input folder, use the Terminal to navigate to the folder where you've placed the program and type
 
 	python rehersify.py
 
 ## Change log
-###alpha 0.1 (released 2015-07-04)
-- Added the basic functionality.
+### alpha 0.2 (released 2015-07-12)
+- Implemented a behind the scenes function so that every input file is converted to a mono file (regardless if it already is a mono file or not).
+- Added an automatic scaling to avoid clipping in the final mix.
+- Added stereo feature: Voices can now be spread out in stereo space.
+
+### alpha 0.1 (released 2015-07-04)
+- Added the basic functionality: Taking a number of sound files and combining them into rehersal files where the volume is mixed differently.
--- rehersify.py
+++ rehersify.py
@@ -3,51 +3,71 @@
 # Global structure of the program:
 # 0. Import necessary libraries
 # 1. Set global variables
-# 2. Create lists
-# 3. Create sox commands
-# 	3.1 Individual rehersal files - volume increased. 
-# 	3.2 Individual rehersal files - channel muted.
-#	3.3 Tutti files.
-# 4. Run sox commands.
+# 3. Create lists
+# 4. Create sox commands
+#	4.0 Define functions.
+#	4.1 Go through each song individual song.
+# 		4.2 Create individual rehersal files - volume increased. 
+# 		4.3 Create individual rehersal files - channel muted.
+#		4.4 Create tutti file.
+# 5. Run sox commands.
+# 6. Normalize all files.
 
-###################################
-## 0. Import necessary libraries ##
-###################################
+#####################################
+### 0. Import necessary libraries ###
+#####################################
 import sys
 import os
 
-#############################
-## 1. Set global variables ##
-#############################
-# TO-DO: Create better handling of 
+###############################
+### 1. Set global variables ###
+###############################
 inputFolder = "./input/"
 outputFolder = "./output/"
-volumeFactor=1
-panFactor=1
-outputFormat="aif"
+outputFormat = "aif"
+temporaryFileName = "tmp.aif"
+volumeDownScale = 0.90
 
-#####################
-## 2. Create lists ##
-#####################
 
-# TO-DO: Add something that checks if the files are formated correctly.
+##########################################
+### 2. Convert all input files to mono ###
+##########################################
+for file in os.listdir(inputFolder):
+    if file.endswith((".aif",".wav")):
+    	soxCommand = "sox \"" + inputFolder + file + "\" \"" + inputFolder + temporaryFileName + "\" remix -"
+    	os.system(soxCommand)
+    	os.remove(inputFolder + file)
+    	os.rename(inputFolder + temporaryFileName, inputFolder + file)
 
+
+#######################
+### 3. Create lists ###
+#######################
 # Create a list of the file names for all files.
-fileNamesList=[]
+fileNamesList = []
 # Create a list of the file endings for all files.
-fileEndingsList=[]
+fileEndingsList = []
 # Create a list of the song names for all files.
-songNamesList=[]
+songNamesList = []
 # Create a list of the voice names for all files.
-voiceNamesList=[]
+voiceNamesList = []
 # Create a list of the voice numbers for all files.
-voiceNumbersList=[]
+voiceNumbersList = []
+# Create a list of the voice placement (relative to all other voices) for all files.
+voicePlacementsList = []
 
 # Fill in all the files created above.
 for file in os.listdir(inputFolder):
     if file.endswith((".aif",".wav")):
-        songName,voiceAndFileEnding=file.split(' @ ')
-        voice,ending=voiceAndFileEnding.split('.')
+        songName,voiceInformationAndFileEnding=file.split(' @ ')
+        voiceInformation,ending=voiceInformationAndFileEnding.split('.')
+        # IF $ IS IN voiceInformation
+        if '$' in voiceInformation:
+        	voice,voicePlacement=voiceInformation.split(' $ ')
+        	voicePlacementsList.append(voicePlacement)
+        else:
+        	voice = voiceInformation
+        	voicePlacementsList.append("center")
         fileNamesList.append(file)
         fileEndingsList.append(ending)
         songNamesList.append(songName)
@@ -61,72 +81,151 @@ for file in os.listdir(inputFolder):
         	voiceNumbersList.append('NA')
 
 # Create a list of unique song names.
-uniqueSongNamesList=set(songNamesList)
+uniqueSongNamesList = set(songNamesList)
 
 # Create a list of number of voices for each unique song.
 numberOfVoicesForUniqueSongNamesList=[]
 for song in uniqueSongNamesList:
     numberOfVoicesForUniqueSong=0
-    for i in range(1,len(fileNamesList)):
+    for i in range(0,len(fileNamesList)):
         if songNamesList[i] == song:
             numberOfVoicesForUniqueSong=numberOfVoicesForUniqueSong+1
     numberOfVoicesForUniqueSongNamesList.append(numberOfVoicesForUniqueSong)
 
-############################
-## 3. Create sox commands ##
-############################
+# Create a list of number of uniqe voice placements for each unique song.
+numberOfUniqueVoicePlacementsForUniqueSongNamesList = []
+for song in uniqueSongNamesList:
+	voicePlacementsForUniqueSongList = []
+	for i in range(0,len(fileNamesList)):
+		if songNamesList[i] == song:
+			voicePlacementsForUniqueSongList.append(voicePlacementsList[i])
+	# Count instances of voice placements for each song if center placement is removed.
+	tmp = [value for value in voicePlacementsForUniqueSongList if value != "center"]
+	numberOfUniqueVoicePlacementsForUniqueSongNamesList.append(len(set(tmp)))
+
+##############################
+### 4. Create sox commands ###
+##############################
+# 4.0 Define functions.
+
+#################################
+# Function: generateSoxCommands #
+#################################
+# Description: Generates a set of sox commands (one for each voice, unless 'volumeFactor' is set to 1, in which case only one command is created) used to build the individual rehersal files.
+# Input:
+#	volumeFactor: A scaling factor deciding how much higher the main voice should be compared to the other voices. Can take on a number between 0 (the main voice is as high as all the other voice) and 1 (the main voice is the only voice that can be heard). If set to "mute", the main voice will be muted. 
+# 	panFactor: A scaling factor deciding how spread out all the voices will be. Can take on a number between 0 (all the voices are centered) and 1 (the voices are maximally spread out).
+def generateSoxCommands(volumeFactor,panFactor):
+	outputList = []
+	partCounter = 0
+	leftChannelMix = ""
+	rightChannelMix = ""
+	soxCommand = "sox -M"
+	for b in range (0,len(fileNamesList)):
+		# Main voice case.
+		if fileNamesList[b] == fileNamesList[a]:
+			soxCommand = soxCommand + " \"" + inputFolder + fileNamesList[b] + "\""
+			partCounter = partCounter + 1
+			if partCounter != 1:
+				leftChannelMix = leftChannelMix + ","
+				rightChannelMix = rightChannelMix + ","
+			leftChannelMix = leftChannelMix + str(partCounter)
+			rightChannelMix = rightChannelMix + str(partCounter)
+			if volumeFactor == "mute":
+				leftChannelMix = leftChannelMix + "v0"
+				rightChannelMix = rightChannelMix + "v0"
+			else:
+				panning = panFactorCalculator(panFactor,voicePlacementsList[b],numberOfUniqueVoicePlacementsForUniqueSongNamesList[uniqueSongCounter],"left")
+				leftChannelMix = leftChannelMix + "v" + str(panning * volumeDownScale)
+				panning = panFactorCalculator(panFactor,voicePlacementsList[b],numberOfUniqueVoicePlacementsForUniqueSongNamesList[uniqueSongCounter],"right")
+				rightChannelMix = rightChannelMix + "v" + str(panning * volumeDownScale)
+		# Other voice case.
+		elif songNamesList[b] == songNamesList[a]:
+			soxCommand = soxCommand + " \"" + inputFolder + fileNamesList[b] + "\""
+			partCounter = partCounter + 1
+			if partCounter != 1:
+				leftChannelMix = leftChannelMix + ","
+				rightChannelMix = rightChannelMix + ","
+			if volumeFactor == "mute":
+				volume = 1
+			else:
+				volume = volumeCalculator(volumeFactor,numberOfVoicesForUniqueSongNamesList[uniqueSongCounter])
+			panning = panFactorCalculator(panFactor,voicePlacementsList[b],numberOfUniqueVoicePlacementsForUniqueSongNamesList[uniqueSongCounter],"left")
+			leftChannelMix = leftChannelMix + str(partCounter) + "v" + str(panning * volume * volumeDownScale)
+			panning = panFactorCalculator(panFactor,voicePlacementsList[b],numberOfUniqueVoicePlacementsForUniqueSongNamesList[uniqueSongCounter],"right")
+			rightChannelMix = rightChannelMix + str(partCounter) + "v" + str(panning * volume * volumeDownScale)
+	soxCommand = soxCommand + " \"" + outputFolder + songNamesList[a] + " - "
+	if volumeFactor == 1:
+		soxCommand = soxCommand + "Tutti "
+	else: 
+		if volumeFactor == "mute":
+			soxCommand = soxCommand + "Sans "
+		soxCommand = soxCommand + voiceNamesList[a]
+		if voiceNumbersList[a] is not 'NA':
+			soxCommand = soxCommand + "-" + voiceNumbersList[a]
+	soxCommand = soxCommand + "." + outputFormat + "\"" + " remix -p " + leftChannelMix + " " + rightChannelMix
+	soxCommandsList.append(soxCommand)
+
+#################################
+# Function: panFactorCalculator #
+#################################
+# Description: Calculates the panning value for an individual voice based on the input values.
+# Input:
+#	panFactor: The desired amount of spread between the different voices. Can take a value between 0 (meaning that all voices are placed in the center) and 1 (meaning that voices are spread out as much as possible).
+#	currentVoicePlacement: The voice placement that the current voice has.
+#	numberOfUniqueVoicePlacements: The number of unique voice placements there are in the song in total.
+#	channel: A variable that says whether the calculation should be done for the left or the right channel. Can take on the values "left" or "right".
+def panFactorCalculator(panFactor,currentVoicePlacement,numberOfUniqueVoicePlacements,channel):
+	if currentVoicePlacement == "center":
+		return(0.5)
+	else:
+		currentVoicePlacement = int(currentVoicePlacement)
+	if channel == "left":
+		return(0.5+(panFactor/2)-(currentVoicePlacement-1)*(panFactor/(numberOfUniqueVoicePlacements-1)))
+	elif channel == "right":
+		return(0.5-(panFactor/2)+(currentVoicePlacement-1)*(panFactor/(numberOfUniqueVoicePlacements-1)))
+	else:
+		print("Error!")
+
+##############################
+# Function: volumeCalculator #
+##############################
+def volumeCalculator(volumeFactor,numberOfPartsForSong):
+	volumeFactor = float(volumeFactor)
+	numberOfPartsForSong = float(numberOfPartsForSong)
+	return(volumeFactor**numberOfPartsForSong)
+
+# 4.1 Go through each individual song.
 soxCommandsList = []
+uniqueSongCounter = -1
 # Loop for each unique song.
 for uniqueSong in uniqueSongNamesList:
-	# Create high and low volume values for this song.
-	# TO-DO: Generate these commands from input values.
-	highVolume=0.80
-	lowVolume=0.125
-	# Used to make the tutti files.
-	standardVolume=0.75
-	# Used to make the muted files.
-	zeroVolume=0
-	standardPlusVolume=0.80
-	# 3.1 Create individual rehersal files for each voice of the song where the voice in question is higher than the others.
+	uniqueSongCounter = uniqueSongCounter + 1
+	# 4.1 Create individual rehersal files for each voice of the song where the voice in question is higher than the others.
 	for a in range(0,len(fileNamesList)):
 		if songNamesList[a] == uniqueSong:
-			soxCommand = "sox -m"
-			for b in range (0,len(fileNamesList)):
-				if fileNamesList[b] == fileNamesList[a]:
-					soxCommand = soxCommand + " -v " + str(highVolume) + " \"" + inputFolder + fileNamesList[b] + "\""
-				elif songNamesList[b] == songNamesList[a]:
-					soxCommand = soxCommand + " -v " + str(lowVolume)  + " \"" + inputFolder + fileNamesList[b] + "\""
-			soxCommand = soxCommand + " \"" + outputFolder + songNamesList[a] + " - " + voiceNamesList[a] 
-			if voiceNumbersList[a] is not 'NA':
-				soxCommand = soxCommand + "-" + voiceNumbersList[a]
-			soxCommand = soxCommand + "." + outputFormat + "\""
-			soxCommandsList.append(soxCommand)
-	# 3.2 Create individual rehersal files for each voice of the son where the voice in question is muted.
+			generateSoxCommands(0.6,0)
+			
+	# 4.2 Create individual rehersal files for each voice of the song where the voice in question is muted.
 	for a in range(0,len(fileNamesList)):
 		if songNamesList[a] == uniqueSong:
-			soxCommand = "sox -m"
-			for b in range (0,len(fileNamesList)):
-				if fileNamesList[b] == fileNamesList[a]:
-					soxCommand = soxCommand + " -v " + str(zeroVolume) + " \"" + inputFolder + fileNamesList[b] + "\""
-				elif songNamesList[b] == songNamesList[a]:
-					soxCommand = soxCommand + " -v " + str(standardPlusVolume)  + " \"" + inputFolder + fileNamesList[b] + "\""
-			soxCommand = soxCommand + " \"" + outputFolder + songNamesList[a] + " - " + "Sans " + voiceNamesList[a] 
-			if voiceNumbersList[a] is not 'NA':
-				soxCommand = soxCommand + "-" + voiceNumbersList[a]
-			soxCommand = soxCommand + "." + outputFormat + "\""
-			soxCommandsList.append(soxCommand)
-	# 3.3 Create a tutti file where all the voices of the song have the same volume.
-	soxCommand = "sox -m"
-	for c in range(0,len(fileNamesList)):
-		if songNamesList[c] == uniqueSong:
-			soxCommand = soxCommand + " -v " + str(standardVolume) + " \"" + inputFolder + fileNamesList[c] + "\""
-	soxCommand = soxCommand + " \"" + outputFolder + uniqueSong + " - Tutti." + outputFormat + "\""
-	soxCommandsList.append(soxCommand)
+			generateSoxCommands("mute",1)
+
+	# 4.3 Create a tutti file of the song.
+	for a in range(0,len(fileNamesList)):
+		if songNamesList[a] == uniqueSong:
+			generateSoxCommands(1,0.8)
 	
-#########################
-## 4. Run sox commands ##
-#########################
+###########################
+### 5. Run sox commands ###
+###########################
 # Uncomment the line below to print all the generated Sox commands.
 # print("\n".join(soxCommandsList))
 for soxCommand in soxCommandsList:
+ 	print(soxCommand)
  	os.system(soxCommand)
+
+
+##############################
+### 6. Normalize all files ###
+##############################
